0387018305676ee592ff479528c5f800154fa38a
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-05 23:21:13 +0530

    new

--- Routers/Students.js
+++ Routers/Students.js
@@ -73,7 +73,7 @@ router.get("/all",async (req,res)=>{
             return res.status(400).json({data:"No details provided"});
         }
         const result =await deleteStudentData(id);
-        res.status(200).json({data:{result:result,message:"Deleted successfully"}})
+        res.status(200).json(result)
         
 
     } catch (error) {
